585b725155757f47c8785b8691331f235a92c5c3
rppsack <rppsack@gmail.com>
2025-03-12 20:46:41 +0200

    Debug Build font

--- install.sh
+++ install.sh
@@ -3,22 +3,27 @@
 # Create a temporary directory
 TEMP_DIR=$(mktemp -d)
 echo "TEMP_DIR installed successfully!"
+sleep 5
 
 # Download the font zip file
 wget -O "$TEMP_DIR/font.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip"
 echo "Wget installed successfully!"
+sleep 5
 
 # Unzip the font file
 unzip "$TEMP_DIR/font.zip" -d "$TEMP_DIR"
 echo "Unzip installed successfully!"
+sleep 5
 
 # Move the font files to the system fonts directory
 sudo mv "$TEMP_DIR"/*.{ttf,otf} /usr/local/share/fonts/
 echo "sudo installed successfully!"
+sleep 5
 
 # Update the font cache
 fc-cache -f -v
 echo "fc-cache installed successfully!"
+sleep 5
 
 # Clean up
 rm -rf "$TEMP_DIR"
