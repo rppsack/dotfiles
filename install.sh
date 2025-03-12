8924daf5cb2fa99d100e1f1e3be1dec9c9e1b306
rppsack <rppsack@gmail.com>
2025-03-12 16:28:15 +0200

    Fix Build font dir

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ curl -o ~/.fonts/MesloLGS_NF_Bold.ttf https://github.com/romkatv/powerlevel10k-m
 curl -o ~/.fonts/MesloLGS_NF_Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
 curl -o ~/.fonts/MesloLGS_NF_BoldItalic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
+sudo apt install fontconfig
 fc-cache
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp ~/dotfiles/p10k.zsh ~/p10k.zsh
 cp ~/dotfiles/.zshrc ~/.zshrc
-exec .zshrc
+exec zsh
